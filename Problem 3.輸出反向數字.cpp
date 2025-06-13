51c3160b3ed6ce48ca51a7237dc154b0ac83ce81
E0206 <131318394+E0206@users.noreply.github.com>
2025-06-13 12:35:25 +0800

    Create Problem 3.輸出反向數字.cpp

---
+++ Problem 3.輸出反向數字.cpp
@@ -0,0 +1,15 @@
+//https://etutor2.itsa.org.tw/mod/topics/view.php?id=43056
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+	string num;
+    cin >> num;
+    cout << num[3] << ',' << num[2] << ',' << num[1] <<',' << num[0] << endl;
+	
+	
+	return 0;
+}
