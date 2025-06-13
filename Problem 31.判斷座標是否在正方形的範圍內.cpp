d36f93b3d0ede840bcf6c484690de7f48f23a9cf
E0206 <131318394+E0206@users.noreply.github.com>
2025-06-13 14:08:55 +0800

    Create Problem 31.判斷座標是否在正方形的範圍內.cpp

---
+++ Problem 31.判斷座標是否在正方形的範圍內.cpp
@@ -0,0 +1,15 @@
+//https://etutor2.itsa.org.tw/mod/topics/view.php?id=43088
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int x, y;
+    cin >> x >> y;
+    if(x <= 100 && y <= 100 )cout <<"inside" << endl;
+    else cout << "outside" << endl;
+	
+	
+	return 0;
+}
