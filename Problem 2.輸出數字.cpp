10ace233ca550d6ca52373ffeed66748ff6de7d6
E0206 <131318394+E0206@users.noreply.github.com>
2025-06-13 12:33:01 +0800

    Create Problem 2.輸出數字.cpp

---
+++ Problem 2.輸出數字.cpp
@@ -0,0 +1,15 @@
+//https://etutor2.itsa.org.tw/mod/topics/view.php?id=43055
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+	string num;
+    cin >> num;
+    cout << num[0] << endl << num[1] << endl << num[2] << endl << num[3] << endl;
+	
+	
+	return 0;
+}
