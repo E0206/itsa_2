d1b6ebc47f81e2e28b0525448fb767b157bf770b
E0206 <131318394+E0206@users.noreply.github.com>
2025-06-13 12:28:03 +0800

    Create Problem 1.打招呼程式.cpp

---
+++ Problem 1.打招呼程式.cpp
@@ -0,0 +1,12 @@
+//https://etutor2.itsa.org.tw/mod/topics/view.php?id=43054
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+    string name;
+    cin >> name;
+    cout << "Hello " << name << endl;
+    return 0;
+}
